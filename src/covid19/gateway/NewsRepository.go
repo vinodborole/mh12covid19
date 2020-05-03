8b47decfe34168e4c306f0868279806551d7f04f
vinodborole <vborole@gmail.com>
2020-05-03 12:59:11 +0530

    news api rest formatting

--- src/covid19/gateway/NewsRepository.go
+++ src/covid19/gateway/NewsRepository.go
@@ -13,7 +13,7 @@ func (dbRepo *DatabaseRepository) AddNews(news *domain.News) (*database.News, er
 	var DBNews database.News
 	u.Copy(&DBNews, news)
 	if len(news.Date) > 0 {
-		DBNews.CreatedAt, _ = time.Parse("2006-01-02", news.Date)
+		DBNews.CreatedAt, _ = time.Parse("2006-01-02 15:04:05", news.Date)
 	}
 	return dbRepo.AddNewsDB(&DBNews)
 }
@@ -31,6 +31,6 @@ func (dbRepo *DatabaseRepository) AddNewsDB(news *database.News) (*database.News
 //GetNews get news by create date
 func (dbRepo *DatabaseRepository) GetNews(createDate string, endDate string) ([]database.News, error) {
 	var DBNews []database.News
-	err := dbRepo.GetDBHandle().Where("date(created_at) >= ? and date(created_at) <= ?", createDate, endDate).Find(&DBNews).Error
+	err := dbRepo.GetDBHandle().Where("date(created_at) >= ? and date(created_at) <= ?", createDate, endDate).Order("created_at desc").Limit(10).Find(&DBNews).Error
 	return DBNews, err
 }
